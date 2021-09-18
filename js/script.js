30996338f31e39c0d294067b56ed05fbfec7b22b
jepkess <biivincent1997@gmail.com>
2021-09-18 16:32:29 +0300

    fix back the script.js file

--- js/script.js
+++ js/script.js
@@ -10,62 +10,6 @@ Order.prototype.fullorder = function () {
 
 
 $(document).ready(function () {
-    $("form#myform").submit(function(event) {
-        event.preventDefault();
-
-        var inputtedname = $("input#name").val();
-        var inputtedsize = $("input#size").val();
-        var inputtedtopping = $("input#topping").val();
-        var inputtedcrust = $("input#crust").val();
-
-        var fullOrder= new Order(inputtedsize, inputtedtopping, inputtedcrust);
-
-
-
-        
-        var sizeprice, toppingprice, crustprice = 0;
-        // sizeprice logic conditions
-        if (inputtedsize == "large") {
-            sizeprice = 2500;
-        } else if (inputtedsize == "medium") {
-            sizeprice = 1000;
-        }
-        else if (inputtedsize == "small"){
-            sizeprice = 500;
-        }
-         // toppingprice logic conditions
-
-        if (inputtedtopping == "large") {
-            sizeprice = 200;
-        } else if (inputtedtopping == "medium"){
-            sizeprice= 150;
-        
-        }else if (inputtedtopping == "small"){
-            sizeprice=100;
-        }
-         // crustprice logic conditions
-         if (inputtedcrust == "large"){
-             sizeprice=150;
-         }else if (inputtedcrust == "medium"){
-             sizeprice=100;
-            
-         } else if (inputtedcrust == "small"){
-             sizeprice=50;
-         }
-
-         const total = sizeprice + toppingprice + crustprice;
-    
-         $("ol#fullorder").append("<li><span class='orderlist'>" + fullorder.fullorder() + "</span>" + "<span id='price'> -(" + sizeprice + "+" + toppingprice + "+" + crustprice + ")=" + total + "/=</span></li>");
-         var totalcost=0;
-         
-
- 
-    })
-    
-});
-
-
-$(document).ready(function () {
     $("#makeorderbtn").on("click", function () {
         $("form#myform").slideDown();
         $(this).text("Make another order");
@@ -75,6 +19,78 @@ $(document).ready(function () {
         $("form#myform").slideUp();
 
     })
-    
-    
+    $("#check").on("click", function () {
+        $(".cart").show();
+        $("form#myform").hide();
+
+
+
+    })
+
+
+});
+
+
+$(document).ready(function () {
+$("form#myform").submit(function (event) {
+    event.preventDefault();
+
+    var inputtedname = $("input#name").val();
+    var inputtedsize = $("input#size").val();
+    var inputtedtopping = $("input#topping").val();
+    var inputtedcrust = $("input#crust").val();
+
+    var fullorder = new Order(inputtedsize, inputtedtopping, inputtedcrust)
+
+
+
+
+    var sizeprice, toppingprice, crustprice = 0;
+    // sizeprice logic conditions
+    if (inputtedsize == "large") {
+        sizeprice = 2500;
+    } else if (inputtedsize == "medium") {
+        sizeprice = 1000;
+    } else if (inputtedsize == "small") {
+        sizeprice = 500;
+    }
+    // toppingprice logic conditions
+
+    if (inputtedtopping == "large") {
+        sizeprice = 200;
+    } else if (inputtedtopping == "medium") {
+        sizeprice = 150;
+
+    } else if (inputtedtopping == "small") {
+        sizeprice = 100;
+    }
+    // crustprice logic conditions
+    if (inputtedcrust == "large") {
+        sizeprice = 150;
+    } else if (inputtedcrust == "medium") {
+        sizeprice = 100;
+
+    } else if (inputtedcrust == "small") {
+        sizeprice = 50;
+    }
+
+    const total = sizeprice + toppingprice + crustprice;
+
+
+    $("ol#fullorder").append("<li><span class='orderlist'>" + fullorder.fullorder() + "</span>" + "<span id='price'> -(" + sizeprice + "+" + toppingprice + "+" + crustprice + ")=" + total + "/=</span></li>");
+    var totalcost = 0;
+    var totalcost = totalcost + total;
+
+    $("#totalcost").append(totalcost);
+    $("#somename").append(inputtedname);
+    // $("#place").append(inputtedlocation);
+    $(".name").hide();
+    $("#name").append(inputtedname).val("");
+});
+
+
+
+
+
+
 });
