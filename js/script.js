a41b19688e2ee054c917dcbd320007e601618cf0
jepkess <biivincent1997@gmail.com>
2021-09-18 13:42:12 +0300

    declaring the parameters in the constructor

--- js/script.js
+++ js/script.js
@@ -7,3 +7,17 @@ function Order(name, size, toppings, crust) {
 Order.prototype.fullorder = function () {
     return this.this.size + " ," + this.toppings + "," + this.crust;
 }
+
+
+$(document).ready(function () {
+    $("form#myform").submit(function(event) {
+        event.preventDefault();
+
+        var inputtedname = $("input#new-name").val();
+        var inputtedsize = $("input#new-size").val();
+        var inputtedtopping = $("input#new-topping").val();
+        var inputtedcrust = $("input#new-crust").val();
+
+
+    }
+})
